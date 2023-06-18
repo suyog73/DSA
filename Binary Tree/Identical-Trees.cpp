a6619ebf99f4ca5328adfecd6bab64d226270e74
SUYOG PATIL <80512884+suyog73@users.noreply.github.com>
2023-06-18 08:52:10 +0530

    Create Identical-Trees.cpp

---
+++ Binary Tree/Identical-Trees.cpp
@@ -0,0 +1,20 @@
+// Check whether two trees are identical or not
+// TC => O(n)
+// SC => O(n) => Recursion Stack
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL and q == NULL)
+            return true;
+        
+        if(p == NULL or q == NULL)
+            return false;
+
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+    }
+};
